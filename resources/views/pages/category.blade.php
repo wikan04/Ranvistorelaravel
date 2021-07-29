c900d03b305ae8d95865120464634bd571fe1fb2
wikan04 <wikan@student.gunadarma.ac.id>
2021-07-29 22:19:24 +0700

    Penggantian mata uang pada all product

--- resources/views/pages/category.blade.php
+++ resources/views/pages/category.blade.php
@@ -78,7 +78,7 @@
                             {{ $product->name }}
                         </div>
                         <div class="products-price">
-                            ${{ $product->price }}
+                            Rp.{{ $product->price }}
                         </div>
                     </a>
                 </div>
