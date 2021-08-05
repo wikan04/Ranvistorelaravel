fdbcfb77cce347cd5361d4f45028ab4f9c464459
wikan04 <wikan@student.gunadarma.ac.id>
2021-08-05 11:55:15 +0700

    Membenahi view sukses

--- resources/views/pages/success.blade.php
+++ resources/views/pages/success.blade.php
@@ -12,18 +12,18 @@
             <div class="col-lg-6 text-center">
               <img src="/images/success.svg" alt="" class="mb-4" />
               <h2>
-                Transaction Processed!
+                Transaksi sedang di proses!
               </h2>
               <p>
                 Silahkan tunggu konfirmasi email dari kami dan kami akan
                 menginformasikan resi secept mungkin!
               </p>
               <div>
-                <a href="/dashboard.html" class="btn btn-success w-50 mt-4">
-                  My Dashboard
+                <a href="{{ route('dashboard') }}" class="btn btn-success w-50 mt-4">
+                  Dashboard Saya
                 </a>
-                <a href="/index.html" class="btn btn-signup w-50 mt-2">
-                  Go To Shopping
+                <a href="{{ route('home') }}" class="btn btn-signup w-50 mt-2">
+                  Kembali berbelanja
                 </a>
               </div>
             </div>
